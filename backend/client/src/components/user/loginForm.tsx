cec13dee5a20b3d505c381c5bbad40ce62600970
Rehan <rehanpradhan10@gmail.com>
2025-03-03 10:39:22 +0545

    login form logic fixed

--- backend/client/src/components/user/loginForm.tsx
+++ backend/client/src/components/user/loginForm.tsx
@@ -63,7 +63,7 @@ export const LoginFrom = () => {
                     className="text-blue-600 hover:text-blue-700 focus:text-blue-700
                       active:text-blue-800 duration-200 transition ease-in-out"
                   >
-                    New to petConnect? Register for an account!
+                    New to petHeaven? Register for an account!
                   </Link>
                 </div>
 
